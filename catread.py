6d98cb430bd86a65532fac940cda327d07cb97c6
Loic Le Tiran <cbienpourtoi@gmail.com>
2014-03-23 18:31:34 -0300

    initial project version

---
+++ catread.py
@@ -0,0 +1,8 @@
+''' Tool forreading catalogs from the Alhambra Survey '''
+''' https://cloud.iaa.csic.es/alhambra/catalogues/ '''
+
+import numpy as np
+import scipy
+import matplotlib.pyplot as plt
+
+	
