d466611fe31576ea6c04caa105173506e6e3620d
Loic Le Tiran <cbienpourtoi@gmail.com>
2014-03-27 20:33:08 -0300

    First implementation. Reads a catalog. Basic procedure for "where" and histogram plots.

--- catread.py
+++ catread.py
@@ -1,8 +1,50 @@
 ''' Tool forreading catalogs from the Alhambra Survey '''
-''' https://cloud.iaa.csic.es/alhambra/catalogues/ '''
+''' These catalogues are hosted on: https://cloud.iaa.csic.es/alhambra/catalogues/ '''
+''' To use the catalogues, see Molino 2013 (soon 2014): http://arxiv.org/abs/1306.4968 (pdf: http://arxiv.org/pdf/1306.4968v2) '''
+
 
 import numpy as np
-import scipy
+#import scipy
 import matplotlib.pyplot as plt
+from astropy.table import Table
 
-	
+
+# Due to a bug in astropy.table (see :
+# http://stackoverflow.com/questions/22617428/overflowerror-python-int-too-large-to-convert-to-c-long-with-astropy-table
+# and
+# https://github.com/astropy/astropy/pull/2234 )
+# I truncate the first line of my catalogs until the bug is corrected.
+# Truncated catalog name has a "MYVERSION." prefix.
+catalog_filename = '/home/loic/data/alhambra/catalogs/MYVERSION.alhambra.F02P01C01.ColorProBPZ.cat'
+#catalog_filename = '/home/loic/data/alhambra/catalogs/alhambra.F02P01C01.ColorProBPZ.cat'
+
+
+t = Table.read(catalog_filename, format='ascii')
+
+print "There are "+ str(len(t))+" elements in this catalogue"
+
+
+# Selects only the objects that are not duplicated, or, if they are, selects only the best SNR object.
+t_noduplicates = t[:][np.where(t['DupliDet_Flag']==0)]
+print "There are "+ str(len(t_noduplicates))+" elements when low SNR duplicates are deleted"
+
+# Selects only the objects that are not likely to be stars.
+t_noduplicates = t[:][np.where(t['DupliDet_Flag']==0)]
+print "There are "+ str(len(t_noduplicates))+" elements when low SNR duplicates are deleted"
+
+
+
+
+plt.hist(t['stell'], bins = 10)
+plt.show()
+
+
+
+#x = np.genfromtxt(catalog_filename, dtype=['i8','i'])#, comments='#')#, names=True)
+
+#print x[1]
+
+
+#with open(catalog_filename) as file:
+#    for line in file:
+#        # whatever you wanted to do with each line
