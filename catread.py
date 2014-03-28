aff9b8332e2d4e6e78cfbc9f57e211addf07d757
Loic Le Tiran <cbienpourtoi@gmail.com>
2014-03-28 20:36:28 -0300

    Improved the plots for stellaricity study

--- catread.py
+++ catread.py
@@ -7,6 +7,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from astropy.table import Table
 import scipy
+from matplotlib.colors import LogNorm
+
+plot_extension = ".png"
+plot_directory = "plots/"
+def savemyplot(name):
+	fig.savefig(plot_directory+name+plot_extension)
+	return
+
+
 
 
 # Due to a bug in astropy.table (see :
@@ -25,24 +34,60 @@ print "There are "+ str(len(t))+" elements in this catalogue"
 
 
 # Selects only the objects that are not duplicated, or, if they are, selects only the best SNR object.
-t_noduplicates = t[:][np.where(t['DupliDet_Flag']==0)]
-print "There are "+ str(len(t_noduplicates))+" elements when low SNR duplicates are deleted"
+t = t[:][np.where(t['DupliDet_Flag']==0)]
+print "There are "+ str(len(t))+" elements when low SNR duplicates are deleted"
 
-# Selects only the objects that are not likely to be stars.
-t_noduplicates = t[:][np.where(t['DupliDet_Flag']==0)]
-print "There are "+ str(len(t_noduplicates))+" elements when low SNR duplicates are deleted"
-
-
+## Selects only the objects that are not likely to be stars.
+#t_noduplicates = t[:][np.where(t['DupliDet_Flag']==0)]
+#print "There are "+ str(len(t_noduplicates))+" elements when low SNR duplicates are deleted"
 
 
+####################################
+# A short study about stellaricity #
+####################################
+
+# Catalog definitions:
+# 'stell' corresponds to SExtractor stellaricity
+# 'Stellar_Flag' is a much better indicator that takes in account many parameters: F814W, FWHM, J-Ks, F489W-F814W. 
+#    See Molino's paper, section "Star/galaxy separation".
+
+
+# SExtractor 'stell' parameter
+fig = plt.figure()
+plt.xlabel("SExtractor stellaricity parameter 'stell' (1=star ; 0=galaxy)")
+plt.title("Distribution of SExtractor stellaricity parameter 'stell'")
+plt.ylabel("Number of occurences")
 plt.hist(t['stell'], bins = 10)
 #plt.show()
+savemyplot("Distribution_parameter_stell")
+plt.close()
 
+
+# Molino's 'Stellar_Flag' parameter
+fig = plt.figure()
+plt.xlabel("Molino's stellaricity parameter 'Stellar_Flag' (1=star ; 0=galaxy)")
+plt.title("Distribution of Molino's stellaricity parameter 'Stellar_Flag'")
+plt.ylabel("Number of occurences")
 plt.hist(t['Stellar_Flag'], bins = 10)
-#plt.show()
-
-plt.plot(t['stell'], t['Stellar_Flag'])
 plt.show()
+savemyplot("Distribution_parameter_stellar_flag")
+plt.close()
+
+'''
+
+fig = plt.figure()
+plt.hist2d(t['stell'], t['Stellar_Flag'], bins=40, norm=LogNorm())
+plt.colorbar()
+#plt.show()
+plt.close()
+
+fig = plt.figure()
+plt.plot( t['F814W'], t['stell'],'.')
+plt.plot( t['F814W'], t['Stellar_Flag'], '.')
+#plt.show()
+plt.close()
+
+'''
 
 #x = np.genfromtxt(catalog_filename, dtype=['i8','i'])#, comments='#')#, names=True)
 
